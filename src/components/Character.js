25dc7ad314c93ae023ee992986b4899fc51b9141
Hansanca <hansanca@gmail.com>
2022-06-21 15:37:06 -0400

    done

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+
+
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+font-size: large;
+color: green;
+`
+
+
+
+
+
+
+const Character = (props) => {
+    return <StyledDiv>{props.name}</StyledDiv>
+}
+
+export default Character
